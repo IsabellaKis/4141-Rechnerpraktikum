248bc70f89098e3de5d77229018ac1a8060b9ec6
h1550955 <h1550955@white4.wu-wien.ac.at>
2017-03-27 15:32:37 +0200

    commit project for the first time

--- src/java/RegistCode.java
+++ src/java/RegistCode.java
@@ -5,8 +5,39 @@ import javax.faces.event.ActionEvent;
 
 @ManagedBean
 public class RegistCode {
+ 
+    String labelName="hhhhhh";
+    boolean editMode=false;
+    private String password5;
     
-    public void buttonAction(ActionEvent actionEvent) {
+    
+    public void buttonAction(String benutzername, String passwort, String passwortWdh) {
+        labelName=benutzername;
+        editMode=true;
+        setName();
+        setEditMode();
         
+        if(!passwort.equals(passwortWdh)){
+        passwortWarn();
+            }
+        
+        }
+        //toDo das von oben löschen
+        //toDo benutzername checken falls != datenbank dann ok sonst fehlermeldung
+        //toDo passwort mit passwortwdh prüfen falls gleich dann ok sonst fehlermeldung
+    
+    public String setName(){
+        return labelName;
     }
+    public boolean setEditMode(){
+        return editMode;
+    }
+    public void passwortWarn() {
+       FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Achtung!", "Passwörter stimmen nicht überein"));
+    }
+    public void benutzernameWarn() {
+       FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Achtung!", "Benutzername bereits vergeben"));
+    }
+    
+
 }

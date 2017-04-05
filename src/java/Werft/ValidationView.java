14f4e63b5f2ba6c10e8ea418c5a109880afdb7b7
h1551285 <h1551285@white20.wu-wien.ac.at>
2017-04-05 18:53:37 +0200

    Updated Werft

---
+++ src/java/Werft/ValidationView.java
@@ -0,0 +1,156 @@
+package Werft;
+
+/* @author h1551285 */
+ 
+import javax.faces.bean.ManagedBean;
+ 
+@ManagedBean
+public class ValidationView {
+     
+    private String schiffsname;
+    private Integer sfm;
+    private Integer m;
+    private Integer lp;
+    private Integer lwl;
+    private Integer amm;
+    private Integer lbm;
+    private Integer ksm;
+    private Integer ksv;
+    private Integer rpmm;
+    private Integer ddp;
+    private Integer dds;
+    private Integer lm;
+    
+ 
+    public String getSchiffsname() {
+        return schiffsname;
+    }
+    public void setSchiffsname(String Schiffsname) {
+        this.schiffsname = Schiffsname;
+    }
+    
+    
+    
+    
+    public Integer getSfm() {
+        return sfm;
+    }
+    public void setSfm(Integer sfm) {
+        this.sfm = sfm;
+    }    
+    
+    
+    
+    
+    public Integer getM() {
+        return m;
+    }
+    public void setM(Integer m) {
+        this.m = m;
+    }    
+    
+    
+    
+    
+    public Integer getLp() {
+        return lp;
+    }
+    public void setLp(Integer lp) {
+        this.lp = lp;
+    }    
+    
+    
+    
+    
+    public Integer getLwl() {
+        return lwl;
+    }
+    public void setLwl(Integer lwl) {
+        this.lwl = lwl;
+    }    
+    
+    
+    
+    
+    public Integer getAmm() {
+        return amm;
+    }
+    public void setAmm(Integer amm) {
+        this.amm = amm;
+    }    
+    
+    
+    
+    
+    public Integer getLbm() {
+        return lbm;
+    }
+    public void setLbm(Integer lbm) {
+        this.lbm = lbm;
+    }    
+    
+    
+    
+    
+    public Integer getKsm() {
+        return ksm;
+    }
+    public void setKsm(Integer ksm) {
+        this.ksm = ksm;
+    }    
+    
+    
+    
+    
+    public Integer getKsv() {
+        return ksv;
+    }
+    public void setKsv(Integer ksv) {
+        this.ksv = ksv;
+    }    
+    
+    
+    
+    
+    public Integer getRpmm() {
+        return rpmm;
+    }
+    public void setRpmm(Integer rpmm) {
+        this.rpmm = rpmm;
+    }    
+    
+    
+    
+    
+    public Integer getDdp() {
+        return ddp;
+    }
+    public void setDdp(Integer ddp) {
+        this.ddp = ddp;
+    }    
+    
+    
+    
+    
+    public Integer getDds() {
+        return dds;
+    }
+    public void setDds(Integer dds) {
+        this.dds = dds;
+    }  
+    
+    
+    
+    
+    public Integer getLm() {
+        return lm;
+    }
+    public void setLm(Integer lm) {
+        this.lm = lm;
+    }  
+}
+    
+    
+    
+    
+    
